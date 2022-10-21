769829b6942f231581b6ac64968cdde5a4fe76e6
RexShack <rexshackuk@gmail.com>
2022-10-21 15:27:06 +0100

    Update player.lua

--- server/player.lua
+++ server/player.lua
@@ -784,6 +784,7 @@ local playertables = { -- Add tables as needed
 	{ table = "bank_accounts" },
 	{ table = "bank_cards" },
 	{ table = "bank_statements" },
+	{ table = "horses" },
 	{ table = "players" },
 	{ table = "player_contacts" },
 	{ table = "playerclothe" },
